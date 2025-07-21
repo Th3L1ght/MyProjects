fb9b4b308b3c01eb1c48eb64412dee0461000305
Th3L1ght <141656697+Th3L1ght@users.noreply.github.com>
2025-07-21 16:07:25 +0300

    feat: Create an analyzer for array statistics
    This project implements a console utility that generates an array
    of random integers and calculates its basic statistics.
    
    Key features include:
    - Calculation of minimum, maximum, and total sum of array elements.
    - Efficient analysis performed in a single pass (O(n) complexity).
    - Use of the 'long' data type for the sum to prevent overflow
      with large arrays or values.
    - Logic is separated into methods for generation, analysis, and
      output.

---
+++ ArrayDataAnalyzer/ArrayDataAnalyzer/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ArrayDataAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = ArrGenerator();
+
+            ArrAnalyze(arr);
+        }
+
+        static int[] ArrGenerator()
+        {
+            Random rd = new Random();
+            int[] arr = new int[rd.Next(1, 46000)];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rd.Next(Int32.MinValue, Int32.MaxValue);
+            }
+            return arr;
+        }
+
+        static void ArrAnalyze(int[] arr)
+        {
+            int minNum = Int32.MaxValue, maxNum = Int32.MinValue;
+            long sum = 0;
+            foreach (int num in arr)
+            {
+                sum += num;
+                if (num < minNum)
+                    minNum = num;
+                if (num > maxNum)
+                    maxNum = num;
+            }
+
+            ArrStatsOutput(arr.Length, minNum, maxNum, sum);
+        }
+
+        private static void ArrStatsOutput(int length, int minNum, int maxNum, long sum)
+        {
+            Console.WriteLine($"The array have {length} elements.");
+            Console.WriteLine($"Minimum value in the array is {minNum}");
+            Console.WriteLine($"Maximum value in the array is {maxNum}");
+            Console.WriteLine($"Sum of all values in the array is {sum}");
+        }
+    }
+}
