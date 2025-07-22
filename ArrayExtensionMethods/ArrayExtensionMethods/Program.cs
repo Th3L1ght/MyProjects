e6776f03dd5d0fb2c56a3eb6c0fc205dd266014d
Th3L1ght <141656697+Th3L1ght@users.noreply.github.com>
2025-07-22 17:17:06 +0300

    feat: Implement utility methods for arrays
    This project creates a static helper class to demonstrate two
    powerful C# features for working with arrays and method arguments.
    
    Key features implemented:
    - An extension method 'FindAndReplace' that adds new functionality
      directly to the int[] type.
    - A method 'CalculateSum' that uses the 'params' keyword to accept
      a variable number of integer arguments.

---
+++ ArrayExtensionMethods/ArrayExtensionMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ArrayExtensionMethods
+{
+    static class ArrayHelper
+    {
+        public static int CalculateSum(params int[] nums)
+        {
+            return nums.Sum();
+        }
+
+        public static void FindAndReplace(this int[] arr, int oldValue, int newValue)
+        {
+            bool wasFound = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == oldValue)
+                {
+                    arr[i] = newValue;
+                    wasFound = true;
+                }
+            }
+            if(!wasFound)
+                Console.WriteLine($"Couldn't find the {oldValue} in the array");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple tests
+            Console.WriteLine(ArrayHelper.CalculateSum(1, 2, 3));
+            Console.WriteLine(ArrayHelper.CalculateSum(5, 10, 15, 20));
+
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            arr.FindAndReplace(5, 55);
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
